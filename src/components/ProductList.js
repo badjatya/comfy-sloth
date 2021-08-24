8510eaf0bf12ee8cbb252c05c30ee62b8732c1c2
Archit Badjatya <architj240@gmail.com>
2021-08-24 17:51:47 +0530

    Styled ListView and ProductList Component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,22 @@ import GridView from "./GridView";
 import ListView from "./ListView";
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext();
+  const { filtered_products: products, grid_view } = useFilterContext();
+
+  // if There is no products
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: "none" }}>
+        Sorry, no products matched your search...
+      </h5>
+    );
+  }
+
+  // If GridView is false than ListView Will be Shown
+  if (grid_view === false) {
+    return <ListView products={products} />;
+  }
+
   return <GridView products={products} />;
 };
 
